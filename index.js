5881133a629c868a62c33ec33b6e4f5794364f2a
aayushsapkota9 <aayushsapkota9@gmail.com>
2023-05-26 00:44:24 +0545

    Frontend Complete

---
+++ index.js
@@ -0,0 +1,23 @@
+function add(a,b)
+{
+    return a+b;
+}
+function subtract(a,b)
+{
+    return a-b;
+}
+function muliply(a,b)
+{
+    return a*b;
+}
+function divide(a,b)
+{
+    return a/b;
+}
+function calculate()
+{
+
+
+let operation;
+operation=document.addEventListener(".add");
+}
